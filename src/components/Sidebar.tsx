9a2137088a089f81ac5b30419b00c5fb4ea25f8b
Edmilson Soares <esbnet@gmail.com>
2022-06-25 18:41:05 -0300

    feat: include graphql-codegen implementation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/genarated";
 import { Lesson } from "./Lesson";
-
-const GET_LESSON_QUERY = gql`
-    query {
-        lessons ( orderBy: availableAt_ASC, stage: PUBLISHED ) {
-            id
-            title
-            slug
-            lessonType
-            availableAt
-        }
-    }
-`
-
-interface GetLessonQueryResponse {
-    lessons: {
-        id: string;
-        title: string;
-        slug: string;
-        lessonType: 'live' | 'class';
-        availableAt: string;
-    }[]
-}
 
 export function Sidebar() {
 
-    const { data } = useQuery<GetLessonQueryResponse>(GET_LESSON_QUERY);
+    const { data } = useGetLessonsQuery();
 
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
